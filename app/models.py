98e52435d5855f9a7f0d1a0064a2021e961b8988
harshitraj33 <harshitpriv.3@gmail.com>
2026-02-16 16:25:53 +0530

    update

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.CharField(max_length=100)
+    course = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
