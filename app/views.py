32123d23684ab24d4184f26939f163ad8e6f95ad
harshitraj33 <harshitpriv.3@gmail.com>
2026-02-17 09:13:47 +0530

    update

--- app/views.py
+++ app/views.py
@@ -7,6 +7,6 @@ from .serializers import StudentSerializer
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    http_method_names = ['get', 'post', 'head', 'options']
+    http_method_names = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options']
     
 
